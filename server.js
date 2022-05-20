885572474ce8310ef74c4749b26dfddf23774fc0
streetCoder <streetcoder99@gmail.com>
2022-05-20 11:36:59 +0100

    Use express to serve static files(trivial)

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var path = require("path");
 var http = require("http");
 
 var sqlite3 = require("sqlite3");
-var staticAlias = require("node-static-alias");
-
+var express = require("express");
+var app  = express();
 
 
 const DB_PATH = path.join(__dirname,"my.db");
@@ -32,45 +32,57 @@ var SQL3 = {
 	exec: util.promisify(myDB.exec.bind(myDB)),
 };
 
-var fileServer = new staticAlias.Server(WEB_PATH,{
-	cache: 100,
-	serverInfo: "Node Workshop: ex5",
-	alias: [
-		{
-			match: /^\/(?:index\/?)?(?:[?#].*$)?$/,
-			serve: "index.html",
-			force: true,
-		},
-		{
-			match: /^\/js\/.+$/,
-			serve: "<% absPath %>",
-			force: true,
-		},
-		{
-			match: /^\/(?:[\w\d]+)(?:[\/?#].*$)?$/,
-			serve: function onMatch(params) {
-				return `${params.basename}.html`;
-			},
-		},
-		{
-			match: /[^]/,
-			serve: "404.html",
-		},
-	],
-});
-
-var httpserv = http.createServer(handleRequest);
+var httpserv = http.createServer(app);
 
 main();
 
 
 function main() {
+  defineRoutes(app);
   httpserv.listen(HTTP_PORT);
 	console.log(`Listening on http://localhost:${HTTP_PORT}...`);
 }
 
+function defineRoutes() {
+  app.get(/\/get-records\b/, async function getRecords(req, res) {
+    let records = await getAllRecords() || [];
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Cache-Control", "max-age: 0, no-cache");
+    res.writeHead(200);
+    res.end(JSON.stringify(records));
+  });
+
+  app.use(function rewriter(req,res,next) {
+    if (/^\/(?:index\/?)?(?:[?#].*$)?$/.test(req.url)) {
+      req.url = "/index.html";
+    } else if (/^\/js\/.+$/.test(req.url)) {
+      next();
+      return;
+    } else if (/^\/(?:[\w\d]+)(?:[\/?#].*$)?$/.test(req.url)) {
+      let [,basename] = req.url.match(/^\/([\w\d]+)(?:[\/?#].*$)?$/);
+      req.url = `${basename}.html`;
+    }
+    next();
+  });
+
+  var fileServer = express.static(WEB_PATH, {
+    maxAge: 100,
+    setHeaders(res) {
+      res.setHeader("Server", "Node workshop");
+    },
+  });
+
+  app.use(fileServer);
+
+  app.get(/\.html$/,function friendly404(req,res,next){
+		req.url = "/404.html";
+		fileServer(req,res,next);
+	});
+
+}
 async function handleRequest(req, res) {
   if (/\/get-records\b/.test(req.url)) {
+    await delay(1000);
     let records = await getAllRecords();
     res.writeHead(200, {
       "Content-type": "application/json",
@@ -80,7 +92,7 @@ async function handleRequest(req, res) {
   } else fileServer.serve(req, res);
 }
 
-async function getAllRecords() {
+async function getAllRecords() { 
 	var result = await SQL3.all(
 		`
 		SELECT
